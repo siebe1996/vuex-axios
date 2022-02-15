fb191872847b04a2ad7a608460e4f4d53d124607
Siebe Van de Voorde <siebe.vandevoorde@student.odisee.be>
2022-02-15 10:29:49 +0100

    adding base files

--- vue-essentials/src/App.vue
+++ vue-essentials/src/App.vue
@@ -1,11 +1,36 @@
 <script>
-
+const emoji = ["🍒", "🍋", "🍆", "🍑"];
+export default {
+  data() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  methods: {},
+};
 </script>
 
 <template>
-
+  <main class="container">
+    <div class="machine">
+      <h1>Hello slotmachine</h1>
+      <div class="slots">
+        <div class="slot">
+          <span>❓</span>
+        </div>
+        <div class="slot">
+          <span>❓</span>
+        </div>
+        <div class="slot">
+          <span>❓</span>
+        </div>
+      </div>
+      <button>PLAY!</button>
+      <div lang="fr">Rien ne va plus</div>
+    </div>
+  </main>
 </template>
 
 <style lang="scss">
-
+@import "./assets/base.css";
 </style>
